908ef8a284b2f6124bbb90787dd868fdea4b37eb
edinson.rey4933@gmail.com <edinson.rey4933@gmail.com>
2023-10-03 02:16:52 -0500

    avance

--- logica.js
+++ logica.js
@@ -5,6 +5,18 @@
         variable[2] = 'Tijera';
         aleatorio   = Math.floor(Math.random() * 3);
         console.log( variable[aleatorio]);
+        return variable[aleatorio];
     }
     
     getComputerChoice()
+    
+    function game(playerSelection,computerSelection){
+      
+      if (playerSelection == computerSelection) {
+        console.log("empate");
+      }
+      
+      console.log (getComputerChoice().toUpperCase())
+      
+    }
+    game("PIEDRA",getComputerChoice().toUpperCase());
