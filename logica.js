d972a2abadb2b01a9ab2707467620152dd89d4c6
edinsonrey4933 <edinson.rey4933@gmail.com>
2023-10-02 22:37:00 -0500

    function getcomputerchoice

---
+++ logica.js
@@ -0,0 +1,15 @@
+    function getComputerChoice () {
+        variable    = new Array();
+        variable[0] = 'Piedra';
+        variable[1] = 'Papel';
+        variable[2] = 'Tijera';
+        aleatorio   = Math.floor(Math.random() * 3);
+        console.log( variable[aleatorio]);
+    }
+    
+    getComputerChoice()
+    getComputerChoice()
+    getComputerChoice()
+    getComputerChoice()
+    getComputerChoice()
+    getComputerChoice()
